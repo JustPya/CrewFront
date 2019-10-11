7a548abd7e2deae9b9d3f52685677a7e999b44a7
Fernanda Moreno <fernandamoreno494@gmail.com>
2019-10-11 02:19:00 -0500

    Register page
    -Changes login page
    -Creatuin register page
    -Inserts images assets folder
    :D

--- Crew/src/app/register/register.page.ts
+++ Crew/src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
 
-  constructor() { }
+  constructor(private router: Router, public menuCtrl: MenuController) {
+    menuCtrl.enable(false, 'first');
+   }
+
+ hideShowPassword() {
+     this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+     this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+ }
 
   ngOnInit() {
   }
