2971971c849678d99e9c4b910bdb9ecb65214b0f
CodeDanielMartinez <danielmartinezdm@hotmail.com>
2019-11-11 18:54:29 -0500

    Dependencies and environments configuration

--- Crew/src/environments/environment.prod.ts
+++ Crew/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCkIGzg5OHt8zjzz8LBcs3J2FyZp9KdylY',
+    authDomain: 'crew-683ea.firebaseapp.com',
+    databaseURL: 'https://crew-683ea.firebaseio.com',
+    projectId: 'crew-683ea',
+    storageBucket: 'crew-683ea.appspot.com',
+    messagingSenderId: '801441567235',
+  }
 };
