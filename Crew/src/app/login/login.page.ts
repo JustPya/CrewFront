53884751077bec10a2b0fc0219ea2553c26bf684
Fernanda Moreno <42256103+JustPya@users.noreply.github.com>
2019-11-11 22:03:12 -0500

    Revert "Login Functionability"

--- Crew/src/app/login/login.page.ts
+++ Crew/src/app/login/login.page.ts
@@ -1,56 +1,46 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { MenuController } from "@ionic/angular";
-
-import { AuthService } from "../services/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"]
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  
+
   username: string;
   password: string;
 
-  passwordType: string = "password";
-  passwordIcon: string = "eye-off";
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    public menuCtrl: MenuController
-  ) {
-    menuCtrl.enable(false, "first");
+  constructor(private router: Router, public menuCtrl: MenuController) {
+    menuCtrl.enable(false, 'first');
+   }
+
+ hideShowPassword() {
+     this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+     this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+ }
+
+ goTo() {
+
+  if (this.username == 'pya@gmail.com' && this.password == '123') {
+    this.router.navigateByUrl('tabs/wallet');
+    this.menuCtrl.enable(true, 'first');
   }
 
-  hideShowPassword() {
-    this.passwordType = this.passwordType === "text" ? "password" : "text";
-    this.passwordIcon = this.passwordIcon === "eye-off" ? "eye" : "eye-off";
-  }
+ }
 
-  loginWithEmail() {
-    console.log(this.username, this.password);
-    this.authService
-      .login(this.username, this.password)
-      .then(res => {
-        this.router.navigateByUrl("/tabs");
-        this.menuCtrl.enable(true, "first");
-        console.log(res);
-      })
-      .catch(err => {
-        console.log("Error");
-        console.log(err);
-      });
-  }
-
-  loginWithGoogle() {}
-
-  hola() {
+  hola(){
     setTimeout(() => {
-      this.router.navigateByUrl("tabs");
+      this.router.navigateByUrl('tabs');
     }, 1000);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
 }
