e552265bb71fd8df6fab6a37b9bca3b28d74e91f
David Segura <daviedu1998@hotmail.com>
2019-11-11 22:53:26 -0500

    Begin create group

--- Crew/src/app/app-routing.module.ts
+++ Crew/src/app/app-routing.module.ts
@@ -1,3 +1,13 @@
+
+
+
+
+
+
+
+
+
+
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -6,7 +16,9 @@ const routes: Routes = [
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
   { path: 'contact-us', loadChildren: './contact-us/contact-us.module#ContactUsPageModule' },
   { path: 'friends', loadChildren: './friends/friends.module#FriendsPageModule' },
-  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'create-group', loadChildren: './create-group/create-group.module#CreateGroupPageModule'}
 
 ];
 
