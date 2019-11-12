344ebb121aaf0b1896ef2f1129151fcf3d33fc7c
CodeDanielMartinez <danielmartinezdm@hotmail.com>
2019-11-11 23:25:45 -0500

    TSLint

--- Crew/src/app/login/login.page.ts
+++ Crew/src/app/login/login.page.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { MenuController } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
-import { AuthService } from "../services/auth.service";
+import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"]
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
   username: string;
   password: string;
 
-  passwordType: string = "password";
-  passwordIcon: string = "eye-off";
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
 
   constructor(
     private authService: AuthService,
     private router: Router,
     public menuCtrl: MenuController
   ) {
-    menuCtrl.enable(false, "first");
+    menuCtrl.enable(false, 'first');
   }
 
   hideShowPassword() {
-    this.passwordType = this.passwordType === "text" ? "password" : "text";
-    this.passwordIcon = this.passwordIcon === "eye-off" ? "eye" : "eye-off";
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
   }
 
   goTo() {
@@ -34,21 +34,14 @@ export class LoginPage implements OnInit {
     this.authService
       .login(this.username, this.password)
       .then(res => {
-        this.router.navigateByUrl("/tabs");
-        this.menuCtrl.enable(true, "first");
+        this.router.navigateByUrl('/tabs');
+        this.menuCtrl.enable(true, 'first');
         console.log(res);
       })
       .catch(err => {
-        console.log("Error");
+        console.log('Error');
         console.log(err);
       });
   }
-
-  hola() {
-    setTimeout(() => {
-      this.router.navigateByUrl("tabs");
-    }, 1000);
-  }
-
   ngOnInit() {}
 }
