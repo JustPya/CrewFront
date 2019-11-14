35eb51ff0decbd1d994e46efcf5eba7a3b3cdcd8
CodeDanielMartinez <danielmartinezdm@hotmail.com>
2019-11-14 16:31:47 -0500

    Auth service for register

--- Crew/src/app/register/register.page.ts
+++ Crew/src/app/register/register.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, LoadingController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 import { UserService } from '../services/user.service';
 import { User } from '../models/User';
@@ -15,57 +14,16 @@ import { AuthService } from '../services/auth.service';
 export class RegisterPage implements OnInit {
   passwordType = 'password';
   passwordIcon = 'eye-off';
-  registerForm: FormGroup;
   users: any;
-
   email: string;
   name: string;
-
-  errorMessage: any;
-  successMessage: any;
   loader: any;
   user: User;
 
   constructor(private router: Router,
               private navCtrl: NavController,
-              private authService: AuthService,
-              private formBuilder: FormBuilder,
               private firebase: UserService,
               private loadingController: LoadingController) {
-
-    this.createFormGroup();
-  }
-
-  createFormGroup() {
-    this.registerForm = this.formBuilder.group({
-      name: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.minLength(6)
-      ])),
-      email: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      ])),
-      password: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
-      ]))
-    });
-    this.errorMessage = {
-      name: [
-        { type: 'required', message: 'Name is required.' },
-      ],
-      email: [
-        { type: 'required', message: 'Email is required.' },
-        { type: 'pattern', message: 'You must provide a valid email.' }
-      ],
-      password: [
-        { type: 'required', message: 'Password is required.' },
-        // tslint:disable-next-line: max-line-length
-        { type: 'pattern', message: 'Your password must be must be at least 6 characters, and must include at least one upper case letter, one lower case letter, and one numeric digit.' }
-      ]
-    };
   }
 
   hideShowPassword() {
@@ -76,7 +34,9 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
-  // Crear usuario en base de datos Firebase
+  /**
+   * Crear usuario en base de datos Firebase
+   */
   createRecord() {
     this.user = new User(this.email, this.name);
     this.firebase.createUser(this.user).then(resp => {
@@ -88,17 +48,9 @@ export class RegisterPage implements OnInit {
       });
   }
 
-  // Registrar con correo electronico en Firebase
-  register(value) {
-    this.showLoader();
-    // this.authService.registerUser(value)
-    //   .then(res => {
-    //     this.createRecord();
-    //     this.goLoginPage();
-    //   }, err => {
-    //     console.log(err);
-    //   });
-  }
+  /**
+   * Create loader for waiting response from service
+   */
   showLoader() {
     this.loader = this.loadingController.create({
       message: 'Wait a second...'
@@ -109,6 +61,10 @@ export class RegisterPage implements OnInit {
       });
     });
   }
+
+  /**
+   * Dismiss loader when service on complete
+   */
   hideLoader() {
     this.loadingController.dismiss();
   }
