ae441e3162bdd095862190a7e9765ff57819d4d3
David Segura <daviedu1998@hotmail.com>
2019-11-30 00:55:01 -0500

    Alert settings

--- Crew/src/app/settings/settings.page.ts
+++ Crew/src/app/settings/settings.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { MenuController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -8,14 +8,61 @@ import { MenuController } from '@ionic/angular';
 })
 export class SettingsPage implements OnInit {
 
-  constructor(private menu: MenuController) { }
+  constructor(private menu: MenuController, private alertController: AlertController) { }
 
   openMenu() {
     console.log("do some");
     this.menu.open("first");
   }
 
+  
   ngOnInit() {
   }
+  /*
+    Method to alert for change the name
+  */
+      async nameAlert() {
+        const alert = await this.alertController.create({
+          header: 'Change username',
+          message: 'Enter new username',
+          inputs: [
+            {
+              name: 'username',
+              type: 'text',
+              placeholder: 'Username',
+            }
+          ],
+          buttons: [{
+            text: 'CANCEL',
+          },
+          {
+            text: 'SAVE',
+          }]
+        });
+        await alert.present();
+      }
+/*
+    Method to alert for change the password
+  */
+      async passwordAlert() {
+        const alert = await this.alertController.create({
+          header: 'Change password',
+          message: 'Enter new password',
+          inputs: [
+            {
+              name: 'password',
+              type: 'password',
+              placeholder: 'Password',
+            }
+          ],
+          buttons: [{
+            text: 'CANCEL',
+          },
+          {
+            text: 'SAVE',
+          }]
+        });
+        await alert.present();
+      }
 
 }
