2a044d9ee9a2ac43d516c241a6759600c5c0ea47
CodeDanielMartinez <danielmartinezdm@hotmail.com>
2019-11-29 19:26:45 -0500

    update to group

--- Crew/src/app/groups/groups.page.ts
+++ Crew/src/app/groups/groups.page.ts
@@ -31,8 +31,6 @@ export class GroupsPage {
         const id = a.payload.doc.id;
         const groupData = a.payload.doc.data() as Group;
         const group = new Group(groupData.name, groupData.description, groupData.date, groupData.participants);
-        this.ids.set(group, id);
-        this.groups.push(group);
         if (groupData.participants) {
           groupData.participants.forEach(participant => {
             console.log(participant);
